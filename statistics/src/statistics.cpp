f5605669d51b5339e6a61bcb43e0003e56251bf5
AngelinaScheck <angelina.scheck@outlook.com>
2017-07-11 19:29:14 +0200

    build folder with first libsvm training added and writing of file for libsvm implemented

--- statistics/src/statistics.cpp
+++ statistics/src/statistics.cpp
@@ -1,5 +1,9 @@
 #include <seqan/vcf_io.h>
 #include <seqan/find.h>
+
+#include <iostream>
+#include <fstream>
+
 #include "statistics.h"
 
 using namespace seqan;
@@ -44,27 +48,66 @@ int main()
     // Copy over header.
     VcfHeader header;
     readHeader(header, vcfIn);
-//     writeHeader(vcfOut, header);
 
     // Copy the file record by record into positive, negative and test-StringSet
-        //Positive: VT=3, negative: VT<2, Test: VT=2
+    //Positive: VT=3, negative: VT<2, Test: VT=2
     VcfRecord record;
     Info_sep infoInStruct;
     int entryNumber=0;
+    
+    //Output File
+    //Libsvm format <label> <index1>:<value1> <index2>:<value2> ... '\n'
+    std::ofstream libsvmformat;
+    libsvmformat.open ("statistics_scale");
+    
     while (!atEnd(vcfIn))
     {
-        //entryNumber++;
-        //std::cout<< "we are in record number " << entryNumber << std::endl;
+        entryNumber++;
         readRecord(record, vcfIn);
         feedInfo(record, infoInStruct);
+        std::cout<< "we are in record number " << entryNumber << std::endl;
+        //write output
+        if(empty(infoInStruct.se)&& empty(infoInStruct.pe) && empty(infoInStruct.re)){
+                continue;
+            }
+        else if(infoInStruct.vt=="3"){
+            libsvmformat<<"1";
+            
+            if(!empty(infoInStruct.se)){
+                libsvmformat << " 1:" << infoInStruct.se;
+            }
+            if(!empty(infoInStruct.pe)){
+                libsvmformat << " 2:" <<infoInStruct.pe;
+            }
+            if(!empty(infoInStruct.re)){
+                libsvmformat << " 3:" <<infoInStruct.re;
+            }
+            libsvmformat<<"\n";
+        }
+        else if(infoInStruct.vt=="1"){
+            libsvmformat<<"-1";
+            
+            if(!empty(infoInStruct.se)){
+                libsvmformat << " 1:" << infoInStruct.se;
+            }
+            if(!empty(infoInStruct.pe)){
+                libsvmformat << " 2:" <<infoInStruct.pe;
+            }
+            if(!empty(infoInStruct.re)){
+                libsvmformat << " 3:" <<infoInStruct.re;
+            }
+            libsvmformat<<"\n";
+        }
+        
+        clearInfo(infoInStruct);
     }
-
     return 0;
 }
 
-
+//==========================================================================================================================
+//save seperated record info
 void feedInfo(seqan::VcfRecord record, Info_sep & infoInStruct){
-    //loop over Info CharString, pattern matching for keywords like SC, copy everything between keyword= and ; intro entry in Info_sep structure
+    //loop over Info CharString, pattern matching for keywords like SC, copy everything between keyword= and ; intro entry in Info_sep structure and write to outputfile for libsvm
     
     //Processing of the Recordinfo
     Finder<CharString> finder(record.info);
@@ -94,11 +137,61 @@ void feedInfo(seqan::VcfRecord record, Info_sep & infoInStruct){
                 break;
             }
         }
-        //get value
+        //save info 
         CharString value = infix(record.info, endPosition(finder), endOfValue);
+        if(infix(finder)=="SC"){
+        infoInStruct.sc=value;
+        }
+        if(infix(finder)==";VT="){
+        infoInStruct.vt=value;
+        }
+        if(infix(finder)==";SE="){
+        infoInStruct.se=value;
+        }
+        if(infix(finder)==";PE="){
+        infoInStruct.pe=value;
+        }
+        if(infix(finder)==";CE="){
+        infoInStruct.ce=value;
+        }
+        if(infix(finder)==";RE="){
+        infoInStruct.re=value;
+        }
+        if(infix(finder)==";RD="){
+        infoInStruct.rd=value;
+        }
+        if(infix(finder)==";GC="){
+        infoInStruct.gc=value;
+        }
+        if(infix(finder)==";CP="){
+        infoInStruct.cp=value;
+        }
+        if(infix(finder)==";SVLEN="){
+        infoInStruct.svlen=value;
+        }
+        if(infix(finder)==";SVTYPE="){
+        infoInStruct.svtype=value;
+        }
+        
         
         //std::cout << position(pattern) << '\t' << infix(finder) << '\t' << infix(record.info, endPosition(finder), endOfValue) << std::endl;
     }
-
-    
+//     libsvmformat.close();
 }
+
+//---------------------------------------------------------------
+//clear up saved separated record info
+void clearInfo(Info_sep & infoInStruct){
+    clear(infoInStruct.sc);
+    clear(infoInStruct.vt);
+    clear(infoInStruct.se);
+    clear(infoInStruct.pe);
+    clear(infoInStruct.ce);
+    clear(infoInStruct.re);
+    clear(infoInStruct.rd);
+    clear(infoInStruct.gc);
+    clear(infoInStruct.cp);
+    clear(infoInStruct.svlen);
+    clear(infoInStruct.svtype);
+}
+
--- statistics/src/statistics.h
+++ statistics/src/statistics.h
@@ -6,35 +6,36 @@
 #include <seqan/sequence.h>
 #include <seqan/vcf_io.h>
 
-using namespace std;
+using namespace seqan;
 
 struct Info_sep
 {
 // example: SC=-1.000000;VT=1;SE=0;PE=1;CE=0;RE=0.019990;RD=53.071429;GC=0.392500;CP=1.966275;SVLEN=-548;SVTYPE=DEL
     //SC
-    float sc;
+    CharString sc;
     //VT
-    unsigned vt;
+    CharString vt;
     //SE
-    unsigned se;
+    CharString se;
     //PE
-    unsigned pe;
+    CharString pe;
     //CE
-    unsigned ce;
+    CharString ce;
     //RE
-    float re;
+    CharString re;
     //RD
-    float rd;
+    CharString rd;
     //GC
-    float gc;
+    CharString gc;
     //CP
-    float cp;
+    CharString cp;
     //SVLEN
-    int svlen;
+    CharString svlen;
     //SVTYPE
-    string svtype;
+    CharString svtype;
 };
 
 void feedInfo(seqan::VcfRecord record, Info_sep & infoInStruct);
+void clearInfo(Info_sep & infoInStruct);
 
 #endif
