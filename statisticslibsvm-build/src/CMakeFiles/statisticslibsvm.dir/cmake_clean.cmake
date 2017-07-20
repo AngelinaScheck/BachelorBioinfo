c50f06e5dcd88734e7e4d07707316af44a3671a0
AngelinaScheck <angelina.scheck@outlook.com>
2017-07-20 14:45:48 +0200

    test results and libsvm library added

---
+++ statisticslibsvm-build/src/CMakeFiles/statisticslibsvm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/statisticslibsvm.dir/statisticslibsvm.cpp.o"
+  "CMakeFiles/statisticslibsvm.dir/svm.cpp.o"
+  "../bin/statisticslibsvm.pdb"
+  "../bin/statisticslibsvm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/statisticslibsvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
